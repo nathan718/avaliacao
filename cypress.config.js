072c1476dc2549b5a3f1c76affbf7b349bdef8b9
Nathan Costa <55506281+nathan718@users.noreply.github.com>
2024-07-07 09:58:20 -0300

    Teste tecnico superteste

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,14 @@
 const { defineConfig } = require('cypress')
 
-module.exports = defineConfig({
+module.exports = {
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-baseUrl: "https://web.superfrete.com/"
-chromeWebSecurity: false
-
-
+      // implement node event listeners here
     },
+    baseUrl: 'https://web.superfrete.com/',
+    pageLoadTimeout: 120000, // Set timeout to 120 seconds
+    defaultCommandTimeout: 30000, // Set default command timeout to 30 seconds
+    waitForAnimations: false // Disable waiting for animations
   },
-})
+  }
+
--- cypress/e2e/testessuper.cy.js
+++ cypress/e2e/testessuper.cy.js
@@ -1,12 +1,15 @@
 /// <reference types="cypress"/>
-// This command will be executed before each test
 Cypress.Commands.add('resetCookies', () => {
     cy.clearCookies();
 });
 
 describe('Calculando envio', () => {
+    beforeEach(() => {
+        cy.resetCookies();
+    });
+
     it('Resultado', () => {
-        cy.visit('https://web.superfrete.com/', { timeout: 10000 });
+        cy.visit('/');
         cy.get('#originPostcode')
             .should('be.visible').click().type('60355305');
         cy.get('#weight')
@@ -28,7 +31,7 @@ describe('Calculando envio', () => {
     });
 
     it('Exceção 1 (remetente incorreto)', () => {
-        cy.visit('https://web.superfrete.com/', { timeout: 10000 });
+        cy.visit('/');
         cy.get('#weight')
             .should('be.visible').click();
         cy.get('[data-value="0.3"]')
@@ -48,7 +51,7 @@ describe('Calculando envio', () => {
     });
 
     it('Exceção 2 (destinatário incorreto)', () => {
-        cy.visit('https://web.superfrete.com/');
+        cy.visit('/');
         cy.get('#originPostcode')
             .should('be.visible').click().type('60355305');
         cy.get('#weight')
@@ -61,9 +64,31 @@ describe('Calculando envio', () => {
             .should('be.visible').click().type('50');
         cy.get('#packageDepth')
             .should('be.visible').click().type('30');
-    
         cy.get('[data-cy="calculator-submit"]')
             .scrollIntoView().should('be.visible').click({ force: true }).wait(10000);
-        cy.get('#destinationPostcode-helper-text').scrollIntoView().should('be.visible', { timeout: 10000 });
+        cy.get('#destinationPostcode-helper-text')
+            .scrollIntoView().should('be.visible', { timeout: 10000 });
+    });
+
+    it('Exceção alertas de tela', () => {
+        cy.visit('/');
+        cy.get('#weight')
+            .should('be.visible').click();
+        cy.get('[data-value="0.3"]')
+            .should('be.visible').click();
+        cy.get('#packageHeight')
+            .should('be.visible').click().type('0.5');
+        cy.get('#packageWidth')
+            .should('be.visible').click().type('8');
+        cy.get('#packageDepth')
+            .should('be.visible').click().type('12');
+        cy.get('[data-cy="calculator-submit"]')
+            .scrollIntoView().should('be.visible').click({ force: true }).wait(10000);
+        cy.get('#destinationPostcode-helper-text')
+            .scrollIntoView().should('be.visible', { timeout: 10000 });
+        cy.get('#packageDepth-helper-text')
+            .scrollIntoView().should('be.visible', { timeout: 10000 });
+        cy.get('#originPostcode-helper-text')
+            .should('be.visible', { timeout: 10000 });       
     });
 });
